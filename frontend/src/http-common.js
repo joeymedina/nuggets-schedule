4add678fbbc94a4539e939d8e028b296620ba025
Joey Medina <joeymedina32@gmail.com>
2023-02-25 00:19:23 -0600

    fix

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+const url = process.env.NODE_ENV === 'production' ? process.env.VUE_APP_BASE_URL : "http://localhost:3000/"
 
 export default axios.create({
-  baseURL: process.env.VUE_APP_BASE_URL || "http://localhost:3000/",
+  baseURL: url,
   headers: {
     "Content-type": "application/json"
   }
