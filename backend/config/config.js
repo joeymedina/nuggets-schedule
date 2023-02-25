96e0ba691ee2d34cec15e09dc8c11b9739d4ec7e
Joey Medina <joeymedina32@gmail.com>
2023-02-24 23:56:20 -0600

    change mysql variables

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,9 +2,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 export const NODE_ENV = process.env.NODE_ENV || 'development';
 export const HOST = process.env.NODE_HOST;
-export const DB_NAME = process.env.DB_NAME;
-export const DB_USER = process.env.DB_USER;
-export const DB_PASS = process.env.DB_PASS;
+export const DB_NAME = process.env.MYSQLDATABASE;
+export const DB_USER = process.env.MYSQLUSER;
+export const DB_PASS = process.env.MYSQLPASSWORD;
 export const PORT = process.env.PORT;
-export const DB_HOST = process.env.DB_HOST;
-export const DB_PORT = process.env.DB_PORT
+export const DB_HOST = process.env.MYSQLHOST;
+export const DB_PORT = process.env.MYSQLPORT
