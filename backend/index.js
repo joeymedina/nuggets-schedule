1fc8e8d21de50d0166129d54e102be0a1bae063c
Joey Medina <joeymedina32@gmail.com>
2023-02-25 20:09:16 -0600

    try env

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,25 @@
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 import express from "express";
-
 import cors from "cors";
 import bodyParser from 'body-parser';
 import Router from "./routes/routes.js";
 import { HOST, PORT } from './config/config.js'
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const path = __dirname + '/public/'
 const app = express();
 
 app.use(express.json());
-
 app.use(express.static(path));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-console.log(process.env.PORT);
 app.use(Router);
+console.log(process.env.REACT_APP_CURRENT_GIT_SHA);
 app.get('/', function (req,res) {
     res.sendFile(path + "index.html");
   });
 
-app.listen(PORT, () => console.log(`Server running at ${HOST}}:${PORT}`));
+app.listen(PORT, () => console.log(`Server running at ${HOST}:${PORT}`));
