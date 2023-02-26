fd4da977e564fbf1379a3ee2e18b5fc6ee02e96d
Joey Medina <joeymedina32@gmail.com>
2023-02-25 18:15:17 -0600

    test

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.static(path));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
+console.log(process.env.PORT);
 app.use(Router);
 app.get('/', function (req,res) {
     res.sendFile(path + "index.html");
