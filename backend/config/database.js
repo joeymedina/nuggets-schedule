824c24b2afd5ec5de49b9bad3789acb692ae9319
Joey Medina <joeymedina32@gmail.com>
2023-04-06 13:19:25 -0500

    test again

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,6 @@
 import mysql from "mysql2"
 import { DB_HOST, DB_NAME, DB_USER, DB_PASS, DB_PORT } from './config.js'
 
-console.log(DB_HOST, DB_NAME, DB_USER, DB_PASS, DB_PORT);
 const db = mysql.createConnection({
     host: DB_HOST,
     user: DB_USER,
